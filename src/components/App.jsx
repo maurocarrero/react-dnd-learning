86529fb979a97f68c9641404e1843936aadecfe3
Mauro Carrero <maurocarrero@lataperainvisible.com.uy>
2019-04-21 10:03:52 -0300

    feat/from-scratch
    add minimal dnd setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,17 @@
 import React from 'react';
-import {
-  StyledDropTargetBox,
-  StyledUl,
-  StyledLi,
-  StyledSpan,
-  StyledWrapper
-} from './Styled';
-import { DragSource } from 'react-dnd';
 
-// Set Item Types
-const ItemTypes = {
-  Name: 'Name'
-};
+import ItemDragSource from './ItemDragSource';
+import ItemDropTarget from './ItemDropTarget';
 
-// Define Drag Source component.
-const Name = ({ children }) => <StyledLi>{children}</StyledLi>;
-const sourceSpec = {
-  beginDrag: () => ({})
-};
-const collectFn = () => ({});
-const NameDragSource = DragSource(ItemTypes.Name, sourceSpec, collectFn)(Name);
-
-const App = () => {
-  return (
-    <StyledWrapper>
-      <StyledDropTargetBox>
-        <StyledSpan>Drag a name here.</StyledSpan>
-      </StyledDropTargetBox>
-      <StyledUl>
-        <NameDragSource>Lucas</NameDragSource>
-        <NameDragSource>Ignacio</NameDragSource>
-        <NameDragSource>Facundo</NameDragSource>
-      </StyledUl>
-    </StyledWrapper>
-  );
-};
+const App = () => (
+  <main>
+    <section>
+      <ItemDropTarget />
+      <ul>
+        <ItemDragSource>Item 1</ItemDragSource>
+      </ul>
+    </section>
+  </main>
+);
 
 export default App;
