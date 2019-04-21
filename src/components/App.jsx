d42b97b39a559673b4fa8b585c20f204f377730e
Mauro Carrero <maurocarrero@lataperainvisible.com.uy>
2019-04-21 10:44:41 -0300

    feat/from-scratch
    access dropped item from target

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,6 +9,8 @@ const App = () => (
       <ItemDropTarget />
       <ul>
         <ItemDragSource>Item 1</ItemDragSource>
+        <ItemDragSource>Item 2</ItemDragSource>
+        <ItemDragSource>Item 3</ItemDragSource>
       </ul>
     </section>
   </main>
--- src/components/ItemDropTarget.jsx
+++ src/components/ItemDropTarget.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { DropTarget } from 'react-dnd';
 import { ItemTypes } from './constants';
 
-const ItemDropTarget = ({ connectDropTarget, isOver }) =>
+const ItemDropTarget = ({ connectDropTarget, canDrop, isOver }) =>
   connectDropTarget(
     <div
       style={{
@@ -12,16 +12,23 @@ const ItemDropTarget = ({ connectDropTarget, isOver }) =>
       <span>
         {isOver ? 'Item is over the target' : 'Item is outside the target'}
       </span>
+      <span>{canDrop ? 'Item can be dropped' : 'Item cannot be dropped'}</span>
     </div>
   );
 
 const targetSpec = {
-  drop: () => {}
+  drop: (_, monitor) => {
+    console.log('Item was dropped', monitor.getItem()); // The item is the one returned by Source.
+  },
+  canDrop: (_, monitor) => {
+    return monitor.isOver();
+  }
 };
 
 const targetCollectFn = (connect, monitor) => ({
   connectDropTarget: connect.dropTarget(),
-  isOver: monitor.isOver()
+  isOver: monitor.isOver(),
+  canDrop: monitor.canDrop()
 });
 
 export default DropTarget(ItemTypes.Item, targetSpec, targetCollectFn)(
